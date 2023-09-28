5365ff5cf446030c294e03a9fe72fe4730b0ef04
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-09-28 11:23:58 +0500

    Task1

---
+++ Tyuiu.AshirbakievAR.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AshirbakievAR.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Алмаз";
+            int age = 18;
+            Console.WriteLine("Здарвствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
