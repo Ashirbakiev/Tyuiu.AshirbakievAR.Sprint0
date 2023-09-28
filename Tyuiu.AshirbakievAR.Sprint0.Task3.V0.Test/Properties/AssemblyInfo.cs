e06c9a6f217a0235d459287aaba3d0dda2926d2e
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-09-28 14:37:20 +0500

    Task3

---
+++ Tyuiu.AshirbakievAR.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AshirbakievAR.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AshirbakievAR.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ddd85c88-ff6b-4f6f-a25d-b8d46782f869")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
